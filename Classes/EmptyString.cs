23a77dc1209e72a29aebd465af5776311ed6b4db
ajayrathi <a_r_rathi@hotmail.com>
2023-09-29 23:41:01 -0400

    Initial checkin for new approch of designing.
    Added new set of classes which can be reused in other project. splited so the the nuket packages can we used for the classes that are not deriving from the base classes of this project.

---
+++ Classes/EmptyString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ShriSoft.SharedKernel.Classes
+{
+    /// <summary>
+    /// Return object of type string and set it as empty.
+    /// This class can be casted to and from string class.
+    /// Value for this class can not be null.
+    /// </summary>
+    public class EmptyString 
+    {
+        private readonly string value = string.Empty;
+
+        public EmptyString(string stringValue)
+        {
+            value = stringValue;
+        }
+
+        public static explicit operator string(EmptyString emptyString)
+        {
+            return emptyString.value;
+        }
+
+        public static implicit operator EmptyString(string value)
+        {
+            return new EmptyString(value);
+        }
+    }
+}
+
---
+++ Classes/PrimaryKey.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShriSoft.SharedKernel.Entities
+{
+    /// <summary>
+    /// Primary key for the entities.
+    /// </summary>
+    /// <typeparam name="TKeyDataType">Data type for the key field</typeparam>
+    public abstract class PrimaryKey<TKeyDataType> : ValueObject
+    {
+        [Key]
+        public TKeyDataType Key { get; set; }
+    }
+}
+
